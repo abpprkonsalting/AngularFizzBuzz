d8474e2c20450f23810abc06a18c1172ff0dea40
Armando Baños <abpprkonsalting@gmail.com>
2021-08-11 17:11:20 -0400

    testing latter ranges

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -50,4 +50,32 @@ describe('AppComponent', () => {
     });
   });
 
+  it ('constants.nonMultiplesOfFive should generate non multiples of five in the range 1 ... 100', () => {
+
+    const multiplesOfFive = [5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100];
+    constants.nonMultiplesOfFive.subscribe((number: number) => {
+      expect(multiplesOfFive).not.toContain(number);
+    });
+  });
+
+  it ('constants.multiplesOfThreeAndFive should generate multiples of three & five in the range 1 ... 100', () => {
+
+    const multiplesOfThree = [3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60,63,66,69,72,75,78,81,84,87,90,93,96,99];
+    const multiplesOfFive = [5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100];
+    let multipleofThreeAndFive = multiplesOfThree.filter(x => multiplesOfFive.includes(x));
+    constants.multiplesOfThreeAndFive.subscribe((number: number) => {
+      expect(multipleofThreeAndFive).toContain(number);
+    });
+  });
+
+  it ('constants.nonMultiplesOfNeitherThreeAndFive should generate non multiples of three & five in the range 1 ... 100', () => {
+
+    const multiplesOfThree = [3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60,63,66,69,72,75,78,81,84,87,90,93,96,99];
+    const multiplesOfFive = [5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100];
+    const multiplesOfBoth = [...multiplesOfThree, ...multiplesOfFive];
+    constants.nonMultiplesOfNeitherThreeAndFive.subscribe((number: number) => {
+      expect(multiplesOfBoth).not.toContain(number)
+    });
+  });
+
 });
