72054193116958d3e227297ce1ff348f22ace2b6
Armando Baños <abpprkonsalting@gmail.com>
2021-08-11 15:53:41 -0400

    testing constants

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,9 @@
 import { TestBed } from '@angular/core/testing';
+import { toArray } from "rxjs/operators";
+
 import { AppComponent } from './app.component';
+import { constants } from './app.constants';
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -22,10 +26,20 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('FizzBuzz');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('FizzBuzz app is running!');
+  it('constants.multiplesOfThree should generate multiples of three in the range 1 ... 100', () => {
+
+    const expected = [3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60,63,66,69,72,75,78,81,84,87,90,93,96,99];
+    constants.multiplesOfThree.pipe(toArray()).subscribe((numbers: number[]) => {
+      expect(numbers).toEqual(expected);
+    });
   });
+
+  it('constants.multiplesOfFive should generate multiples of five in the range 1 ... 100', () => {
+
+    const expected = [5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100];
+    constants.multiplesOfFive.pipe(toArray()).subscribe((numbers: number[]) => {
+      expect(numbers).toEqual(expected);
+    });
+  });
+
 });
