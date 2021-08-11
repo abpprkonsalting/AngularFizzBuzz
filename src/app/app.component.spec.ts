073fc8977dd1a908c00bd8540fbef1eab953eba5
Armando Baños <abpprkonsalting@gmail.com>
2021-08-11 16:37:29 -0400

    test nonMultipleOfThree alt method

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -42,4 +42,12 @@ describe('AppComponent', () => {
     });
   });
 
+  it ('constants.nonMultiplesOfThree should generate non multiples of three in the range 1 ... 100', () => {
+
+    const multiplesOfThree = [3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60,63,66,69,72,75,78,81,84,87,90,93,96,99];
+    constants.nonMultiplesOfThree.subscribe((number: number) => {
+      expect(multiplesOfThree).not.toContain(number);
+    });
+  });
+
 });
