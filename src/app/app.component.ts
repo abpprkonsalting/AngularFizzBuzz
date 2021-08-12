78233312b4f23e856901583678717f21a5662b47
Armando Baños <abpprkonsalting@gmail.com>
2021-08-11 22:43:02 -0400

    printing results to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { toArray } from 'rxjs/operators';
+
+import { FlowResult } from './infrastructure/model/flow.result';
+import { constants } from './app.constants';
+import { MathService } from './services/math/math.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'FizzBuzz';
+  numbers$ : Observable<Array<FlowResult>>;
+
+  constructor(mathService: MathService){
+    this.numbers$ = constants.numbers.pipe(mathService.filterMultipleThree)
+                                      .pipe(mathService.filterMultipleFive)
+                                      .pipe(mathService.filterNonMultiples).pipe(toArray());
+  }
 }
